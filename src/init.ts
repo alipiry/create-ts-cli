0b6337577888001e9f561a9cf196b9a32cff3791
Ali Piry <alipiry@disroot.org>
2019-01-30 23:43:06 +0330

    Update init.ts

--- src/init.ts
+++ src/init.ts
@@ -1,5 +1,36 @@
 #!/usr/bin/env node
 
-export const init = (): void => {
+import fs from 'fs';
+
+export function init(): void {
+	if (!fs.existsSync('example')) fs.mkdirSync('example');
+
+// temp solution
+	fs
+		.writeFile(
+			'example/.gitignore',
+			'',
+			(err) => {
+				if (err) throw err;
+			}
+		)
+
+	fs
+		.writeFile(
+			'example/package.json',
+			'',
+			(err) => {
+				if (err) throw err;
+			}
+		)
+	fs
+		.writeFile(
+			'example/tsconfig.json',
+			'',
+			(err) => {
+				if (err) throw err;
+			}
+		)
+
 	console.log('Initilize ts-cli');
 }
