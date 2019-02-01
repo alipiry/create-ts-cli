4f8c24bf682b3a955a6d6d34a00273ba370021d0
Ali Piry <alipiry@disroot.org>
2019-02-01 23:29:00 +0330

    Fix typos

--- src/init.ts
+++ src/init.ts
@@ -2,19 +2,19 @@ import fs from 'fs';
 import { filesToWrite } from './consts';
 
 export function init(dir: string): void {
-	if (!fs.existsSync(dir)) fs.mkdirSync(dir);
-	if (!fs.existsSync('src')) fs.mkdirSync('src');
-	
-	filesToWrite.forEach(index => {
-		fs.writeFile(
-			`${dir}/${index.name}`,
-			index.data,
-			'utf8',
-			err => {
-				if (err) throw err;
-			}
-		);
-	});
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+  if (!fs.existsSync('src')) fs.mkdirSync('src');
 
-	console.log('Initilize ts-cli');
+  filesToWrite.forEach(index => {
+    fs.writeFile(
+      `${dir}/${index.name}`,
+      index.data,
+      'utf8',
+      err => {
+        if (err) throw err;
+      }
+    );
+  });
+
+  console.log('Initilize ts-cli');
 }
