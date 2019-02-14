665bd97381e94553987686d55383356a87e830ae
Ali Piry <alipiry@disroot.org>
2019-02-14 22:22:22 +0330

    Update init.ts

--- src/init.ts
+++ src/init.ts
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import chalk from 'chalk';
+
 import { filesToWrite } from './consts';
 
 export function init(dir: string): void {
@@ -24,5 +26,10 @@ export function init(dir: string): void {
     }
   );
 
-  console.log('Initilize ts-cli');
+  console.log(chalk.red.bold('ts-cli package initilized!'));
+  console.log(chalk.blue.bold('Change your current working dir to the project'));
+  console.log(chalk.green.bold('Run `yarn` to install dependencies of your project'));
+  console.log(chalk.yellow.bold('Simply `yarn dev` to run your cli app'));
+  console.log(chalk.cyan.bold('For production build, use `yarn build`'));
+
 }
